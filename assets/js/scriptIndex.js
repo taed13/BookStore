b711514580a06adc6c6827e9f1c23098c8bcdd42
taed13 <tiendatpot@gmail.com>
2023-03-28 20:04:11 +0700

    done header section

---
+++ assets/js/scriptIndex.js
@@ -0,0 +1,26 @@
+searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+}
+
+
+
+window.onscroll = () => {
+
+    searchForm.classList.remove('active');
+
+    if (window.scrollY > 80) {
+        document.querySelector('.header .header-2').classList.add('active');
+    } else {
+        document.querySelector('.header .header-2').classList.add('remove');
+    }
+}
+
+window.onload = () => {
+    if (window.scrollY > 80) {
+        document.querySelector('.header .header-2').classList.add('active');
+    } else {
+        document.querySelector('.header .header-2').classList.add('remove');
+    }
+}
