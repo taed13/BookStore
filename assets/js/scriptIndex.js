9994929afccae133e082500071cc06e86c63e6ce
taed13 <tiendatpot@gmail.com>
2023-03-29 21:57:55 +0700

    done login form

--- assets/js/scriptIndex.js
+++ assets/js/scriptIndex.js
@@ -4,7 +4,15 @@ document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
 }
 
+let loginForm = document.querySelector('.login-form-container');
 
+document.querySelector('#login-btn').onclick = () => {
+    loginForm.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick = () => {
+    loginForm.classList.remove('active');
+}
 
 window.onscroll = () => {
 
