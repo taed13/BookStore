3b9a607e94594b7b4b0cace7a614e093920dfc08
taed13 <tiendatpot@gmail.com>
2023-03-30 22:26:41 +0700

    done icons section

--- assets/js/scriptIndex.js
+++ assets/js/scriptIndex.js
@@ -32,3 +32,23 @@ window.onload = () => {
         document.querySelector('.header .header-2').classList.add('remove');
     }
 }
+
+var swiper = new Swiper(".books-slider", {
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
